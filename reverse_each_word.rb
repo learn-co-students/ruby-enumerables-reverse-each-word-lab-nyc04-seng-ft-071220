38b647008f8b00ab8d50d8c2d5b701b04c90c4bc
mirsafirst47 <habib.triande@gmail.com>
2020-07-26 12:46:43 -0400

    Revisited adding a second solution

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,13 @@
 def reverse_each_word(string)
-    string.split.collect {|word| word.reverse}.join(" ")
+    new_str = string.split
+    new_str.collect { |word| word.reverse}.join(" ") 
 end
+
+
+#def reverse_each_word(string)
+   # reverse_word = []
+    #string.split.each do |word|
+        #reverse_word << word.reverse
+    #end
+    #reverse_word.join(" ")
+#end
